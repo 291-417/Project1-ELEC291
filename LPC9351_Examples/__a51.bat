a29de368dc6b76688623cd78b42a95f0988aff33
Ryan Meshulam <ryan@sgoblin.com>
2020-02-02 10:31:29 -0800

    Initial commit

---
+++ LPC9351_Examples/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Examples\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Examples\LPC9351_blinky.asm"
+echo Crosside_Action Set_Hex_File C:\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Examples\LPC9351_blinky.HEX
