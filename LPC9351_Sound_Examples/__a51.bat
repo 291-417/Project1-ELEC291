5ba4e7d89c0b8a2d85d77ae3154c0c058fa50d0a
Ryan Meshulam <ryan@sgoblin.com>
2020-02-03 14:53:37 -0800

    Initial attempt at ADC and DAC integration

---
+++ LPC9351_Sound_Examples/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Sound_Examples\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Sound_Examples\LPC9351_Receiver.asm"
+echo Crosside_Action Set_Hex_File C:\Users\ryan\Documents\2ndYear\ELEC291\Project1\LPC9351_Sound_Examples\LPC9351_Receiver.HEX
--- Project1.asm
+++ Project1.asm
@@ -12,11 +12,36 @@ CCU_RATE    EQU 22050     ; 22050Hz is the sampling rate of the wav file we are 
 CCU_RELOAD  EQU ((65536-((CLK/(2*CCU_RATE)))))
 BAUD        EQU 115200
 BRVAL       EQU ((CLK/BAUD)-16)
+XTAL        EQU 7373000
+
+LCD_RS equ P1.6
+LCD_RW equ P1.4
+LCD_E  equ P1.3
+LCD_D4 equ P0.7
+LCD_D5 equ P0.6
+LCD_D6 equ P0.5
+LCD_D7 equ P0.3
+
+PLAY_BUTTON equ P3.0
 
 FLASH_CE    EQU P2.4
 
 dseg at 30H
 	w:   ds 3 ; 24-bit play counter.  Decremented in CCU ISR.
+  x:   ds 4
+  y:   ds 4
+  bcd: ds 5
+ 
+bseg
+
+mf: dbit 1
+PB0: dbit 1 ; Variable to store the state of pushbutton 0 after calling ADC_to_PB below
+PB1: dbit 1 ; Variable to store the state of pushbutton 1 after calling ADC_to_PB below
+PB2: dbit 1 ; Variable to store the state of pushbutton 2 after calling ADC_to_PB below
+PB3: dbit 1 ; Variable to store the state of pushbutton 3 after calling ADC_to_PB below
+PB4: dbit 1 ; Variable to store the state of pushbutton 4 after calling ADC_to_PB below
+PB5: dbit 1 ; Variable to store the state of pushbutton 5 after calling ADC_to_PB below
+PB6: dbit 1 ; Variable to store the state of pushbutton 6 after calling ADC_to_PB below
 
 cseg
 
@@ -41,10 +66,13 @@ org 0x0023 ; Serial port receive/transmit interrupt vector (not used in this cod
 org 0x005b ; CCU interrupt vector.  Used in this code to replay the wave file.
 	ljmp CCU_ISR
 
-bseg
+
 
 $NOLIST
+$INCLUDE(math32.inc)
+$include(LCD_4bit_LPC9351.inc) ; A library of LCD related functions and utility macros
 $include(sound.inc)
+$include(temppb.inc)
 $LIST
 
 ;---------------------------------;
@@ -79,10 +107,32 @@ MainProgram:
   lcall InitDAC ; Call after 'Ports_Init
 	lcall CCU_Init
 	lcall Init_SPI
+  
+  lcall InitSerialPort
+	lcall InitADC0
+  lcall LCD_4BIT
+
+  ; Set beginning message on LCD
+  Set_Cursor(1, 1)
+  Send_Constant_String(#Title)
+
+  lcall Wait1S ; Wait a bit so PUTTy has a chance to start
+
+  mov dptr, #InitialMessage
+	lcall SendString
 
   clr TMOD20 ; Stop CCU timer
 	setb EA ; Enable global interrupts.
 
 forever:
-
+  lcall Read_Temperature
+  lcall ADC_to_PB
+	lcall Display_PushButtons_ADC
+	jb PLAY_BUTTON, forever
+  Wait_Milli_Seconds(#50)
+  jb PLAY_BUTTON, forever
+  jnb PLAY_BUTTON, $
+  lcall Play_Whole_Memory
   ljmp forever
+
+end
