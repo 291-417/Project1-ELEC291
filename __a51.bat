136982fec496974a0030edd31badab47a5187a29
oakleybr <56177759+oakleybr@users.noreply.github.com>
2020-02-11 13:08:06 -0800

    working variable print for settings.inc

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
-C:
-cd "\Users\ANUSIKA\Documents\Project1-ELEC291\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\ANUSIKA\Documents\Project1-ELEC291\Project1.asm"
-echo Crosside_Action Set_Hex_File C:\Users\ANUSIKA\Documents\Project1-ELEC291\Project1.HEX
+D:
+cd "\Onedrive\UBC\ELEC 291\Project1-ELEC291\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "D:\Onedrive\UBC\ELEC 291\Project1-ELEC291\Project1.asm"
+echo Crosside_Action Set_Hex_File D:\Onedrive\UBC\ELEC 291\Project1-ELEC291\Project1.HEX
