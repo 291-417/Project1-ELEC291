61ef34f86be667b10e17a4cfe2435997854fcf86
Big Yoshi <trvmackay@gmail.com>
2020-02-13 08:49:32 -0800

    Update project1.py

--- project1.py
+++ project1.py
@@ -24,7 +24,7 @@ ave_delta_temp = 0
 ann_delta_t = None
 last_temp_print = 0
 
-xsize=100
+xsize=250
 
 def data_gen():
     t = data_gen.t
@@ -46,18 +46,30 @@ def run(data):
     if t>-1:
         xdata.append(t)
         ydata1.append(y1)
-        if t>xsize: # Scroll to the left.
-            ax.set_xlim(t-xsize, t)
+
+        #Ryan asked for the graph to scale the t axis
+        ax.set_xlim(0,t+10)
+
+        #Makes the graph scroll to the left if t is greater then the max t
+        #if t>xsize: # Scroll to the left.
+            #ax.set_xlim(t-xsize, t)
+
         line1.set_data(xdata, ydata1)
+
         temp_max = 250
         temp_grad = (abs(y1%temp_max)) / temp_max
         red_val = temp_grad
         green_val = 1 - red_val
         
+        position_constant = 10
+
         if(len(ydata1) % 30 == 0) or (y1 > last_temp_print + 10) or (y1 < last_temp_print - 10) :
             last_temp_print = y1
-            ax.annotate('(%s s , %.2f °C)' %(t, y1), xy= (t, y1), textcoords = 'data',color=(red_val,green_val,0.0))
-
+            if ave_delta_temp >= 0:
+                ax.annotate('(%s s , %.2f °C)' %(t, y1), xy= (t, y1), xytext=(t,y1 - position_constant), textcoords = 'data',color=(red_val,green_val,0.0))
+            else:  
+                ax.annotate('(%s s , %.2f °C)' %(t, y1), xy= (t, y1), xytext=(t,y1 + position_constant), textcoords = 'data',color=(red_val,green_val,0.0))
+                
         if ann_delta_t is not None:
                ann_delta_t.remove()
                
@@ -76,16 +88,20 @@ def run(data):
             ave_delta_temp = ( delta_temp1 + delta_temp2 + delta_temp3 ) / 3
             
             list_ave = sum(temp_list) / len(temp_list)
+
+            #if the 
             if list_ave >=150 and ave_delta_temp <= -2 and print_list[4] == 0:
                 ax.annotate("Cooling Down", xy=(t, y1), xycoords=ax.transData,xytext=(t, y1-125), textcoords=ax.transData,arrowprops=dict(arrowstyle="->"), bbox=dict(boxstyle="round", fc='tab:blue'))
+                #write back that the 
                 print_list[4] =1
+
             if list_ave >= 200 and ave_delta_temp >= 1 and print_list[3] == 0:
                 ax.annotate("Entering Reflow", xy=(t, y1), xycoords=ax.transData,xytext=(t, y1-125), textcoords=ax.transData,arrowprops=dict(arrowstyle="->"), bbox=dict(boxstyle="round", fc='tab:red'))
                 print_list[3] =1
             if list_ave >= 150 and ave_delta_temp >= 2 and print_list[2] == 0:
                 ax.annotate("Ramp to Peak", xy=(t, y1), xycoords=ax.transData,xytext=(t, y1-125), textcoords=ax.transData,arrowprops=dict(arrowstyle="->"), bbox=dict(boxstyle="round", fc='tab:green'))
                 print_list[2] =1
-            if list_ave >= 145 and print_list[1] == 0:
+            if list_ave >= 145 and ave_delta_temp >= 2 and print_list[1] == 0:
                 ax.annotate("Entering Preheat Soak", xy=(t, y1), xycoords=ax.transData,xytext=(t, y1-125), textcoords=ax.transData,arrowprops=dict(arrowstyle="->"), bbox=dict(boxstyle="round", fc='tab:gray'))
                 print_list[1] =1
             if list_ave >= 30 and ave_delta_temp>= 2 and print_list[0] == 0:
@@ -96,7 +112,6 @@ def run(data):
 
 def on_close_figure(event):
     sys.exit(0)
-    
 
 data_gen.t = -1
 fig = plt.figure()
