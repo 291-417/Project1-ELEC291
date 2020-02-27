be16f53a80822945ae968618b16365062e19b5d5
Ryan Meshulam <ryan@sgoblin.com>
2020-02-26 16:46:29 -0800

    Added some comments

--- Project1.asm
+++ Project1.asm
@@ -21,12 +21,12 @@ LCD_D4 equ P1.2
 LCD_D5 equ P1.3
 LCD_D6 equ P1.4
 LCD_D7 equ P1.6
-DIP_BUTTON1 equ P3.0
-PLAY_BUTTON equ P0.3
-SOUND equ P2.7
-PWM_PIN equ P0.2
-STARTSTOP equ P2.1
-ABORT equ P0.1
+DIP_BUTTON1 equ P3.0 ; When low (switch thrown), in settings mode
+PLAY_BUTTON equ P0.3 ; Currently acts as screen reset button
+SOUND equ P2.7 ; Connects to the input of the sound op amp
+PWM_PIN equ P0.2 ; Connects to transistor for PWM output, oven is on when low
+STARTSTOP equ P2.1 ; Start button
+ABORT equ P0.1 ; Abort button
 FLASH_CE    EQU P2.4
 
 
@@ -35,26 +35,26 @@ dseg at 30H
   x:   ds 4
   y:   ds 4
   bcd: ds 5
-  sound_start: ds 3
-  pwm_percentage: ds 1
-  state: ds 1
-  overall_time: ds 2
-  state_time: ds 2
-  lmtemp: ds 4
-  thermotemp: ds 4
-  totaltemp: ds 4
-  pwm_counter: ds 1
-  Count1ms: ds 2
-  talker_counter: ds 1
-  temp_truncated: ds 1
-  temp_bcd: ds 2
-  Talk_temp: ds 2
+  sound_start: ds 3 ; Directs sound playing code where to start
+  pwm_percentage: ds 1 ; Set to determine PWM percentage on; range 1-99
+  state: ds 1 ; Current state: options are waiting, ramp to soak, preheat, ramp to peak, reflow, and cooling
+  overall_time: ds 2 ; Time since the start button was pressed, stays at zero in waiting state
+  state_time: ds 2 ; Time since state transition, stays at zero in waiting state
+  lmtemp: ds 4 ; Temperature at the cold junction
+  thermotemp: ds 4 ; Temperature difference between sides of the thermocouple
+  totaltemp: ds 4 ; Temperature at end of thermocouple
+  pwm_counter: ds 1 ; Counts down number of PWM cycles, gets reloaded when it gets to zero with pwm_percentage or 100-pwm_percentage
+  Count1ms: ds 2 ; Milliseconds counter for overall timing
+  talker_counter: ds 1 ; Counts number of cycles through number-saying FSM, more details in sound.inc
+  temp_truncated: ds 1 ; Temperature integer
+  temp_bcd: ds 2 ; Temperature in BCD
+  Talk_temp: ds 2 ; Temperature that is being said -- notusing temp_bcd because temperature can change in the middle of talking
   param_state: ds 1
   soak_temp: ds 1
   soak_time: ds 1
   reflow_temp: ds 1
   reflow_time: ds 1
-  language: ds 1
+  language: ds 1 ; Left as an excersise for the reader
 bseg
 
 mf: dbit 1
@@ -97,12 +97,12 @@ org 0x005b ; CCU interrupt vector.  Used in this code to replay the wave file.
 $NOLIST
 $INCLUDE(math32.inc)
 $include(LCD_4bit_LPC9351.inc) ; A library of LCD related functions and utility macros
-$include(sound.inc)
-$include(temppb.inc)
-$include(pwm.inc)
-$include(fsm.inc)
-$include(secinc.inc)
-$include(settings.inc)
+$include(sound.inc) ; Obama talks here
+$include(temppb.inc) ; Reading temperatures
+$include(pwm.inc) ; PWM for oven control
+$include(fsm.inc) ; Main FSM for oven control
+$include(secinc.inc) ; Seconds increment
+$include(settings.inc) ; Settings menu
 $LIST
 
 ;---------------------------------;
@@ -165,29 +165,30 @@ MainProgram:
   mov P3M2, #00H
 
 
-  mov state, #0
+  mov state, #0 ; Initial state is waiting
   mov param_state, #0
 
+  ; Default parameters usually work
   mov soak_temp, #140
   mov soak_time, #60
   mov reflow_temp, #225
   mov reflow_time, #45
-  mov language, #0
+  mov language, #0 ; ö
   
-  lcall Sound_Start_Init
+  lcall Sound_Start_Init ; Initialize the beginning place in flash to start playing
   lcall Ports_Init ; Default all pins as bidirectional I/O. See Table 42.
-  lcall Double_Clk
+  lcall Double_Clk ; Use fast clock
   lcall InitDAC ; Call after 'Ports_Init
 	
-  lcall CCU_Init
-  lcall Init_SPI
+  lcall CCU_Init ; Initialize talking timer
+  lcall Init_SPI ; Initialize SPI for sound flash
 
-  lcall Timer0_Init
-  lcall Timer1_Init
+  lcall Timer0_Init ; Initialize Timer0 for PWM
+  lcall Timer1_Init ; Initialize Timer1 for seconds incrementing
   
-  lcall InitSerialPort
-  lcall InitADC0
-  lcall LCD_4BIT
+  lcall InitSerialPort ; Initialize serial port for communication with computer
+  lcall InitADC0 ; ADC0 used for push buttons and LM335
+  lcall LCD_4BIT ; Initialize LCD
 
   mov state_time+0, #0
 	mov state_time+1, #0
@@ -209,29 +210,23 @@ MainProgram:
 	setb EA ; Enable global interrupts.
 
   mov a, #34
-  lcall Play_Numbered
+  lcall Play_Numbered ; Startup sound "ha'ha"!
 
 forever:
   jb DIP_BUTTON1, next
-  clr EA
-  lcall ADC_to_PB
-	lcall settings
+  ; If we pass the dipswitch then go to the settings menu
+  clr EA ; Stop interrupts, we don't need to play sound, increment seconds, or use PWM during settings setting
+  lcall ADC_to_PB ; Read the ADC pushbuttons
+	lcall settings ; Open the settings menu
   sjmp forever
 
 next:
-  setb EA
-  ;mov dptr, #Temp
-	;lcall SendString
-  
-  ;mov dptr, #Thermocouple
-	;lcall SendString
-  ;mov b, state_time
-  ;lcall SendHex
-  jb Abort, moveon
+  setb EA ; We definitely want interrupts during normal running
+  jb Abort, moveon ; ABORT BUTTON CHECK - Immediately exit if abort buttom is pushed
   jnb ABort, $  
   mov a, state
   jz moveon
-  mov state, #0x0 
+  mov state, #0x0  ; Move to state zero, fsm will take care of turning stuff off
  
 moveon:
   ;mov a, temp_truncated
@@ -241,13 +236,12 @@ moveon:
   ;mov state, #0
   ;clr SOUND
 moveon2:
-  lcall ADC_to_PB
-  lcall fsm_update
-  lcall update_lcd
-	;lcall Wait1S
+  lcall ADC_to_PB ; KEEP THIS LINE IN - IT DOESN'T WORK IF IT ISN'T THERE
+  lcall fsm_update ; Go through the FSM
+  lcall update_lcd ; Show stuff on the LCD
 	
-  jb second_flag, Every_Second_Stuff
-  jb PLAY_BUTTON, forever
+  jb second_flag, Every_Second_Stuff ; If it's exactly a second, then we do stuff that happens every second
+  jb PLAY_BUTTON, forever ; Reset the screen if button is pressed
   ;Wait_Milli_Seconds(#50)
   ;jb PLAY_BUTTON, forever
   jnb PLAY_BUTTON, $
@@ -257,9 +251,9 @@ moveon2:
 Check_Temperatures:
   ;mov dptr, #Total
   ;lcall SendString
-  lcall Get_Thermocouple
-  lcall Read_Temperature
-  lcall get_total_temp
+  lcall Get_Thermocouple ; Read the thermocouple
+  lcall Read_Temperature ; Read the temperature at the LM335
+  lcall get_total_temp ; Read overall temperature by combining thermocouple and cold junction data
   ;mov b, temp_truncated
   ;lcall SendHex
   ljmp Every_Second_2
@@ -272,11 +266,12 @@ Check_Temperatures:
 Every_Second_Stuff:
   ;mov bcd+1, #0x02
   ;mov bcd+0, #0x53
-  clr second_flag
+  clr second_flag ; Clear the seconds flag so that we don't do this stuff every time
   ;mov b, state_time
   ;lcall SendHex
   ;mov b, reflow_time
   ;lcall SendHex
+  ; Clear the screen
   Set_Cursor(1,1)
   Send_Constant_String(#clear)
   Set_Cursor(2,1)
@@ -284,10 +279,10 @@ Every_Second_Stuff:
   ljmp Check_Temperatures
   ;jnb say_a_number, Check_Temperatures
 Every_Second_2:
-  lcall BCD_To_Sound
-  lcall Say_Stuff_FSM
-  inc state_time
-  inc overall_time+0
+  lcall BCD_To_Sound ; Actually say stuff
+  lcall Say_Stuff_FSM ; Set flag if we want to say stuff
+  inc state_time ; Increase state_time
+  inc overall_time+0 ; Increase overall time
   mov a, overall_time+0
   jnz End_Every_Second
 increment_upper_seconds:
